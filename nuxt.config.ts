72113c717ebbe4c06773f77d7eecbd6b49f8fcb8
ThomasSL27 <thomassohus27@gmail.com>
2026-01-05 09:08:56 +0100

    Fjerne Faceit API ting, da man ikke kan pr. dags dato

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  runtimeConfig: {
-    faceitApiKey: process.env.FACEIT_API_KEY as string,
-  },
   css: [
     '~/assets/css/main.css',
     '@fortawesome/fontawesome-free/css/all.css',
