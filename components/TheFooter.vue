d2472f865e2396b00a46d6942da27bb59c989e2f
TobiasVraa <10632181@ucn.dk>
2026-01-08 15:18:02 +0100

    fix fix glem de andre

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -19,8 +19,8 @@
     <div id="column2">
         <p>Quick Links</p>
         <NuxtLink to="/">Team</NuxtLink>
-        <NuxtLink to="/results">Results</NuxtLink>
         <NuxtLink to="/sashiproleague">Sashi Pro League</NuxtLink>
+        <NuxtLink to="/about">About</NuxtLink>
         <NuxtLink to="/contact">Contact us</NuxtLink>
     </div>
     <div id="column3">
